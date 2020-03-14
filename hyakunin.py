f1c3ff8b5941163495f5ece796321031ffd972fa
Kenichi <newunkn@gmail.com>
2020-03-14 12:52:52 +0900

    first commit

---
+++ hyakunin.py
@@ -0,0 +1,24 @@
+#本より手打ち
+
+import sys
+import urllib.request as req
+import urllib.parse as parse
+
+if len(sys.argv) <= 1:
+    print("USAGE: hyakunin.py (keyword)")
+    sys.exit()
+keyword = sys.argv[1]
+
+API="http://api.aoikujira.com/hyakunin/get.php"
+query={
+    "fmt": "ini",
+    "key": keyword
+}
+params = parse.urlencode(query)
+url = API + "?" + params
+print("url=", url)
+
+with req.urlopen(url) as r:
+    b = r.read()
+    data = b.decode('utf-8')
+    print(data)
